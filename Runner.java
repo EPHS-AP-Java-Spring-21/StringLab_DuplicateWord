8fb7b5070f170dafb73b139a3e90de91b3bc8f11
Jenn Nelson <jnelson@edenpr.org>
2021-02-28 20:24:35 -0600

    Create Runner.java

---
+++ Runner.java
@@ -0,0 +1,10 @@
+public class Runnder{
+  
+ public static void main(String[] args){
+   
+  String[] commonWords = {"the","of","and","a","to","in","is","you","that","it","he","was","for","on","are","as","that","it","he","was","for","with","his","they","I","at","be","this","have","from","that","it","he","was","for","or","one","had","by","word","but","not","do","how","their","what","all","were","we","when","your","can","said","that","it","he","was","for","there","use","an","each","which","she","do","how","their","if","will","up","other","about","out","many","then","them","these","so","some","her","would","make","like","him","into","time","has","look","two","more","write","go","see","number","no","way","could","people","my","than","first","not","what","all","were","we","water","been","call","who","oil","its","now","find","long","down","day","did","get","come","made","may","part"}; 
+   
+   
+ }
+  
+}
